4ccbc801b8f92f1244c3890ed7fd6fa3b8667a8a
Ali Tekdemir <ali.tekdemir@gmail.com>
2024-10-26 03:35:40 +0300

    For Mac users

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = [('resources', ['il_plaka.json'])]  # JSON dosyasını ekliyoruz
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': [],
+    'plist': {
+        'CFBundleName': 'NamazZamanı',
+        'CFBundleVersion': '1.0',
+        'CFBundleIdentifier': 'com.example.myapp',
+        'LSUIElement': True,  # Terminal penceresini gizler
+    },
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
